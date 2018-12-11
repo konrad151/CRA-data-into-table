8c244feed870144835633cded8169886fc5a07a3
Konrad <konrad.k151@gmail.com>
2018-12-11 22:02:25 +0100

    Create Table.js component
    In this component are received data from parent and new state is created from them.
    Here are rendered table rows by renderTableRows function, which map the state and return
    TableRow component for each of objects.
    In table headers are added onClick events which run sortByRows function with parameter depend on clicked element.

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,117 +1,101 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TableRow from '../TableRow/TableRow';
+import styles from './Table.module.scss';
 
-const Table = () => {
-    return (
-        <div className="well">
-            <table className="table">
-                <tbody>
+class Table extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            workers: [],
+            originalWorkers: []
+        }
+    }
+
+    componentWillReceiveProps(props) {
+        this.setState({
+            workers: props.workers,
+            originalWorkers: props.workers
+        });
+    }
+
+    renderTableRows = () => {
+        const allWorkers = [...this.state.workers];
+        return (
+            allWorkers.map((worker, index) => {
+                return (
+                    <tr key={index}>
+                        <TableRow
+                        name={worker.name}
+                        last_Name={worker.last_Name}
+                        date_of_birth={worker.date_of_birth}
+                        position={worker.position}
+                        city={worker.city}
+                        />
+                    </tr>
+                )
+            })
+        )
+    }
+
+    sortByRows(event, sortKey){
+        let presentWorkers = [...this.state.workers];
+        const originalWorkers = [...this.state.originalWorkers];
+
+        const sortByType = (a,b,sortKey) => {
+            let aSortKey;
+            let bSortKey;
+            if (sortKey === 'date_of_birth') {
+                aSortKey = a[sortKey].split('.').reverse().join('');
+                bSortKey = b[sortKey].split('.').reverse().join('');
+            } else {
+                aSortKey = a[sortKey];
+                bSortKey = b[sortKey];
+            }
+            return aSortKey.localeCompare(bSortKey)
+        }
+
+        const sortKeyRowsData = (data) => data.map(item => item[sortKey])
+
+        const sortedOriginalWorkers = [...originalWorkers].sort((a,b) => sortByType(a,b,sortKey));
+        let sortedWorkers = [];
+        if (JSON.stringify(sortKeyRowsData(presentWorkers)) === JSON.stringify(sortKeyRowsData(sortedOriginalWorkers))) {
+            sortedWorkers =  [...presentWorkers].sort((a,b) => sortByType(a,b,sortKey)).reverse();
+        } else {
+            sortedWorkers =  [...presentWorkers].sort((a,b) => sortByType(a,b,sortKey));
+        }
+        this.setState({workers: sortedWorkers});
+        
+        this.renderTableRows();
+    }
+
+    render() {
+        return (
+            <table id={styles.noMoreTables} className="table table-bordered table-striped table-condensed">
+                <thead>
                     <tr>
-                        <th>
+                        <th className={styles.clickable} onClick={e => this.sortByRows(e, 'name')}>
                             Imię
                         </th>
-                        <th>
+                        <th className={styles.clickable} onClick={e => this.sortByRows(e, 'last_Name')}>
                             Nazwisko
                         </th>
-                        <th>
+                        <th className={styles.clickable} onClick={e => this.sortByRows(e, 'date_of_birth')}>
                             Data urodzenia
                         </th>
-                        <th>
+                        <th className={styles.clickable} onClick={e => this.sortByRows(e, 'position')}>
                             Stanowisko
                         </th>
-                        <th>
+                        <th className={styles.clickable} onClick={e => this.sortByRows(e, 'city')}>
                             Miejsce zamieszkania
                         </th>
                     </tr>
-                    <tr>
-                        <td>
-                            Jan
-                        </td>
-                        <td>
-                            Kowalski
-                        </td>
-                        <td>
-                            15.02.1986
-                        </td>
-                        <td>
-                            Project Manager
-                        </td>
-                        <td>
-                            Wrocław
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Anna
-                        </td>
-                        <td>
-                            Nowak
-                        </td>
-                        <td>
-                            25.06.1985
-                        </td>
-                        <td>
-                            Team Leader
-                        </td>
-                        <td>
-                            Wrocław
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            John
-                        </td>
-                        <td>
-                            Doe
-                        </td>
-                        <td>
-                            21.03.1986
-                        </td>
-                        <td>
-                            Senior PHP Devloper
-                        </td>
-                        <td>
-                            Londyn
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Katarzyna
-                        </td>
-                        <td>
-                            Wiśniewska
-                        </td>
-                        <td>
-                            11.03.1994
-                        </td>
-                        <td>
-                            Frontend Developer
-                        </td>
-                        <td>
-                            Katowice
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Jacek
-                        </td>
-                        <td>
-                            Zieliński
-                        </td>
-                        <td>
-                            11.07.1992
-                        </td>
-                        <td>
-                            Software Tester
-                        </td>
-                        <td>
-                            Wrocław
-                        </td>
-                    </tr>
-
+                </thead>
+                <tbody>
+                    {this.renderTableRows()}
                 </tbody>
             </table>
-        </div>
-    );
+        );
+    }
 };
 
 export default Table;
