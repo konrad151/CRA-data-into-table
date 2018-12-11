3f963a3148d58ed8df2cbe1cd088537f0f62f701
Konrad <konrad.k151@gmail.com>
2018-12-11 21:46:27 +0100

    Get data and send state with wokers by props to children component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Table from '../components/Table/Table';
 import '../bootstrap/css/bootstrap.min.css';
 import styles from './App.module.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      workers: []
+    }
+  }
+
+  componentDidMount() {
+    this.getWorkers();
+  }
+
+  async getWorkers() {
+      try {
+          const res = await axios(`/workers`)
+          this.setState({workers: res.data});
+      } catch (error) {
+          console.log(error);
+      }
+  }
+
   render() {
     return (
       <div className={styles.app}>
         <div className="container">
           <div className="row">
             <div className="col-xs-12">
-              <Table/>
+              <Table workers={this.state.workers}/>
             </div>
           </div>
         </div>
